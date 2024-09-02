92f435bb900f5be4f44e64bf6db2f077992f8fe0
serwikk <012sergio210@gmail.com>
2024-09-03 01:41:35 +0200

    acciones añadidas

---
+++ bin/bash/acciones/cocinar.sh
@@ -0,0 +1,5 @@
+
+
+export PYTHONPATH="/home/serwikk/Domotica_Controller_Cocina"
+cd /home/serwikk/Domotica_Controller_Cocina
+python3 /home/serwikk/Domotica_Controller_Cocina/bin/python/cocina/cocinar.py --segundos $1
