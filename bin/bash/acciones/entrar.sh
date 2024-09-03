a7ff85ee5dc5f3547cf4d6db3a0dde4388a934fd
serwikk <012sergio210@gmail.com>
2024-09-03 02:27:20 +0200

    entrar y salir añadidos

---
+++ bin/bash/acciones/entrar.sh
@@ -0,0 +1,5 @@
+
+
+export PYTHONPATH="/home/serwikk/Domotica_Controller_Cocina"
+cd /home/serwikk/Domotica_Controller_Cocina
+python3 /home/serwikk/Domotica_Controller_Cocina/bin/python/comunes/presencia/entrar_en_habitaculo.py
